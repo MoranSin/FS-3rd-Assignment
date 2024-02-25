09d1b5be1264d4cde00673c01da801e074bdb5fa
MoranSin <nurixhbh@gmail.com>
2024-02-25 22:14:36 +0200

    header component

--- middlewares/errorHandler.js
+++
@@ -1,4 +0,0 @@
-exports.errorHandler = (error, req, res, next) => {
-    res.status(error.status || 500)
-    res.json({ message: error.message || 'Internal Server Error' })
-}
---
+++ react-app/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+        <header><div>Damage Reports</div></header>
+        )
+    }
+}
+
+export default Header;
