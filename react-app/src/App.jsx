2e1b1b63fb37747474eb24d3d35e3a181413cf4f
MoranSin <nurixhbh@gmail.com>
2024-02-26 15:01:42 +0200

    form appears when clicking create

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,11 +3,10 @@ import './App.css'
 import Header from './components/header/Header'
 import ButtonList from "./components/ButtonList/ButtonList.jsx";
 import ReportsCon from "./components/reportsCon/reportsCon.jsx";
+import 'semantic-ui-css/semantic.min.css'
 
 
 function App() {
-    // this.state = { editing: false }
-
   return (
     <>
         <Header />
--- react-app/src/components/addButton/AddButton.jsx
+++ react-app/src/components/addButton/AddButton.jsx
@@ -2,10 +2,44 @@ import React , { Component } from 'react';
 import '../ButtonList/crudButton.css'
 import addIcon from '../../assets/addIcon.png'
 
+import { Button, Form } from 'semantic-ui-react'
+
+
+const Create = () => (
+    <Form>
+        <Form.Field>
+            <label>Name</label>
+            <input placeholder='Name' />
+        </Form.Field>
+        <Form.Field>
+            <label>Location</label>
+            <input placeholder='Location' />
+        </Form.Field>
+        <Form.Field>
+            <label>Death Count</label>
+            <input placeholder='Death Count' />
+        </Form.Field>
+        <Form.Field>
+            <label>Damage</label>
+            <input placeholder='Damage' />
+        </Form.Field>
+        <Button type='submit'>Submit</Button>
+    </Form>
+)
 class AddButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { open: false }
+        this.create = this.create.bind(this)
+    }
+
+    create() { this.setState({open: true}) }
     render() {
         return (
-        <button className="crudButton btn"><img src={addIcon}/></button>
+        <div>
+        <button onClick={this.create} className="crudButton btn"><img src={addIcon}/></button>
+        {this.state.open && <Create />}
+        </div>
         )
     }
 }
