12c2d1bb368bd76d18ef493a4dda23dc1f814828
MoranSin <nurixhbh@gmail.com>
2024-02-26 03:24:21 +0200

    added reports

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import ButtonList from "./components/ButtonList/ButtonList.jsx";
+import ReportsCon from "./components/reportsCon/reportsCon.jsx";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
         <Header />
         <ButtonList />
+        <ReportsCon />
     </>
   )
 }
